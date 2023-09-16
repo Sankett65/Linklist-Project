8bd4ffd5b29be78e517846f39183db3683691498
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-09-16 16:32:47 +0530

    Creating Simple Linklist

---
+++ Linklist Project/src/com/bridgrlabz/LinkList.java
@@ -0,0 +1,13 @@
+package com.bridgrlabz;
+
+import java.util.LinkedList;
+
+public class LinkList {
+    public static void main(String[] args) {
+        LinkedList<Integer> list =new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);
+    }
+}
